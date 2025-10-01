c5e386815c77ca9924431d34e2e81e1450d5b65c
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-10-01 19:48:35 +0600

    [example,config]: removed unnecessary config for vue example;

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import babel from 'vite-plugin-babel';
 
 // https://vite.dev/config/
@@ -15,7 +14,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
     babel({
       babelConfig: {
         babelrc: false,
@@ -29,7 +27,6 @@ export default defineConfig({
         ],
       },
     }),
-
   ],
   resolve: {
     alias: {
