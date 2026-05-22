c3b61ce8b94b72a0360416155791664e81f9f769
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-22 13:17:51 +0600

    config: basic eslint config;

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["lib/**", "coverage/**", "examples/**", "node_modules/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "eqeqeq": ["error", "always"],
+      "no-duplicate-imports": "error",
+      "no-console": "off",
+      "no-undef": "off",
+      "no-redeclare": "off",
+      "no-unused-vars": "off",
+      "prefer-const": "error",
+      "@typescript-eslint/consistent-type-imports": ["error", { "prefer": "type-imports" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-redeclare": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ]
+    },
+  },
+  {
+    files: ["__tests__/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+    },
+  },
+];
