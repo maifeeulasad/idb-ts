54d981feae679b317a06f2f67732b823969748b6
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-31 16:12:43 +0600

    [lint]: ran lint on `examples/vue/vite.config.ts`;

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import babel from 'vite-plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: "./build/vue",
+    outDir: './build/vue',
   },
-  base: "./",
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
@@ -18,19 +18,17 @@ export default defineConfig({
       babelConfig: {
         babelrc: false,
         configFile: false,
-        presets: [
-          ["@babel/preset-env", { loose: true }],
-        ],
+        presets: [['@babel/preset-env', { loose: true }]],
         plugins: [
-          ["@babel/plugin-proposal-decorators", { version: "legacy" }],
-          ["@babel/plugin-proposal-class-properties", { loose: true }],
+          ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
+          ['@babel/plugin-proposal-class-properties', { loose: true }],
         ],
       },
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
