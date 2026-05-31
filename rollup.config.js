890b366e29892ca5e711d75223cb2530bf31ab1f
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-31 21:39:21 +0600

    [config]: rollup for index, cjs; declare external;

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,28 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'index.ts',
+  external: ['reflect-metadata', 'tslib'],
   output: [
     {
       file: 'lib/index.esm.js',
       format: 'esm',
     },
+    {
+      file: 'lib/index.js',
+      format: 'esm',
+    },
+    {
+      file: 'lib/index.cjs',
+      format: 'cjs',
+    },
   ],
-  plugins: [typescript()],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      compilerOptions: {
+        declaration: false,
+        module: 'esnext',
+      },
+    }),
+  ],
 };
