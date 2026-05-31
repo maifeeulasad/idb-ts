7c40d820789c8f4851376f63ee1d7db60346bedf
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-31 15:48:21 +0600

    [config,lint]: use single quote for string; run lint on eslint configuration;

--- eslint.config.js
+++ eslint.config.js
@@ -1,83 +1,94 @@
-import js from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import globals from "globals";
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+const quoteRules = {
+  quotes: ['error', 'single', { avoidEscape: true }],
+};
 
 export default [
   {
-    ignores: ["lib/**", "coverage/**", "examples/**", "node_modules/**"],
+    ignores: ['lib/**', 'coverage/**', 'examples/**', 'node_modules/**'],
   },
   js.configs.recommended,
   {
-    files: ["*.ts"],
+    files: ['**/*.{js,mjs,cjs}'],
+    rules: {
+      ...quoteRules,
+    },
+  },
+  {
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: {
         ...globals.browser,
         ...globals.node,
-        IDBObjectStoreParameters: "readonly",
-        IDBIndexParameters: "readonly",
-        IDBTransactionMode: "readonly",
+        IDBObjectStoreParameters: 'readonly',
+        IDBIndexParameters: 'readonly',
+        IDBTransactionMode: 'readonly',
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
     },
     rules: {
-      "eqeqeq": ["error", "always"],
-      "no-duplicate-imports": "error",
-      "no-console": "off",
-      "no-redeclare": "off",
-      "no-unused-vars": "off",
-      "prefer-const": "error",
-      "@typescript-eslint/consistent-type-imports": [
-        "error",
+      ...quoteRules,
+      'eqeqeq': ['error', 'always'],
+      'no-duplicate-imports': ['error', { allowSeparateTypeImports: true }],
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'prefer-const': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
         {
-          prefer: "type-imports",
-          fixStyle: "separate-type-imports",
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
           disallowTypeAnnotations: false
         }
       ],
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-redeclare": "error",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-redeclare': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_"
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_'
         }
       ]
     },
   },
   {
-    files: ["__tests__/**/*.ts"],
+    files: ['__tests__/**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: {
         ...globals.browser,
         ...globals.node,
         ...globals.jest,
-        fail: "readonly",
+        fail: 'readonly',
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
     },
     rules: {
-      "@typescript-eslint/consistent-type-imports": [
-        "error",
+      ...quoteRules,
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
         {
-          prefer: "type-imports",
-          fixStyle: "separate-type-imports",
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
           disallowTypeAnnotations: false
         }
       ]
