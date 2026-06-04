001d63e2ed6fcca701a5c8e6d62871b6d7be16f3
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-06-04 13:16:50 +0600

    [config]: ignore `doc` directory for eslint;

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,7 @@ const lengthRules = {
 
 export default [
   {
-    ignores: ['lib/**', 'coverage/**', 'examples/**', 'node_modules/**'],
+    ignores: ['lib/**', 'coverage/**', 'examples/**', 'node_modules/**', 'docs/**'],
   },
   js.configs.recommended,
   {
